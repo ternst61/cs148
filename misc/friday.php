91aa542010fdb7c66ef7f826390be4c1816d0821
ternst61 <then.ernst@gmail.com>
2015-10-09 14:03:45 -0400

    friday assignment 2

--- misc/friday.php
+++ misc/friday.php
@@ -6,23 +6,39 @@
  */
 include "top.php";
 
+    $num = 10;
+    $start = 1000;
+    
+    if($_GET["num"]){
+        $num = (int) $_GET["num"];
+    }
+    if($_GET["start"]){
+        $start = (int) $_GET["start"];
+    }
+    
 //now print out each record
     $columns = 8;
-    $query = 'SELECT pmkStudentId, fldFirstName, fldLastName, fldStreetAddress, fldCity, fldState, fldZip, fldGender FROM tblStudents ORDER BY fldLastName ASC, fldFirstName ASC LIMIT 10 OFFSET 1000';
+    $query = 'SELECT pmkStudentId, fldFirstName, fldLastName, fldStreetAddress, fldCity, fldState, fldZip, fldGender FROM tblStudents '
+            . 'ORDER BY fldLastName ASC, fldFirstName ASC '
+            . 'LIMIT ' . $num . ' OFFSET ' . $start;
     //$testquery = $thisDatabaseReader->select($query, "", 0, 0, 0, 0, false, false);
     $info2 = $thisDatabaseReader->select($query, "", 0, 1, 0, 0, false, false);
     
     print ' ' . count($info2) . ' records';
+    
+   print '<a href = "friday.php?num='. $num .'&start='. ($start - $num) .'"><button>Previous Page</button></a>' ;
+
+    print '<a href = "friday.php?num='. $num .'&start='. ($start + $num) .'"><button>Next Page</button></a>' ;
     
    print '<table>';
    
    print '<tr><th>Student ID</th><th>First Name</th><th>Last Name</th><th>Street Address</th><th>City</th><th>State</th><th>Zip</th><th>Gender</th></tr>';
 
    
-   $labels = array_filter(array_keys($info2[0]), 'is_string');
+   //$labels = array_filter(array_keys($info2[0]), 'is_string');
    
-   print '<p><pre>';
-   print_r($labels);
+  // print '<p><pre>';
+  // print_r($labels);
     
    
    
@@ -40,9 +56,14 @@ include "top.php";
         }
         print '</tr>';
     }
+    
 
     // all done
     print '</table>';
+    
+    
+
+
    // print '</aside>';
     
     include "footer.php";
